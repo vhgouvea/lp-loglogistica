9de29c69646c55f817dac99a2fc6d3108224db0b
Victor Hugo <vhgouvea@hotmail.com>
2023-08-21 23:07:38 -0300

    estruturando rotas, pastas e removendo arquivos

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { 
+    Container
+ } from './styles';
+
+export function Home() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
