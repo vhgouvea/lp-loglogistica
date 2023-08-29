fcab74d080df274db6c4a26659db60cdc3c72a11
Victor Hugo <vhgouvea@hotmail.com>
2023-08-28 22:59:07 -0300

    adicionado botao whatsapp com chat

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
+import Logo from '../../assets/logo-com-fundo.png'
 
 import { 
     Container
  } from './styles';
+import { Header } from "../../components/Header";
 
 export function Home() {
     return (
         <Container>
-
+            <Header />
+            <FloatingWhatsApp 
+                accountName="Log Transportes"
+                phoneNumber="17996428126" //71988891370
+                avatar={Logo}
+                chatMessage="Olá, como posso ajuda-lo?"
+                allowEsc={true}
+            />
         </Container>
-    )
+    )   
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 `;
