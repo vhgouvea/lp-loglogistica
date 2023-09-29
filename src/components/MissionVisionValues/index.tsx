a661040b267e1bf70315ca78b3eb5c1dcbc506a3
Victor Hugo <vhgouvea@hotmail.com>
2023-09-28 23:18:42 -0300

    adicionado os depoimentos dos clientes e corrigido responsividade do footer

--- src/components/MissionVisionValues/index.tsx
+++ src/components/MissionVisionValues/index.tsx
@@ -10,51 +10,6 @@ import Valores from '../../assets/valores.png';
 import Visao from '../../assets/visao.png';
 
 export function MissionVisionValues() {
-  // return(
-  //   <Container>
-  //     <Card>
-  //       <ContentIcon>
-  //         <img src={Missao} alt="missao" className="w-24 h-24" />
-  //       </ContentIcon>
-  //       <ContentTitle>
-  //         <Title>Missão</Title>
-  //       </ContentTitle>
-  //       <ContentText>
-  //         <Text>Promover soluções em logística integrada e transporte de cargas com qualidade, segurança e pontualidade sempre visando superar as expectativas dos nossos clientes e do mercado.</Text>
-  //       </ContentText>
-  //     </Card>
-  //     <Card>
-  //       <ContentIcon>
-  //         <img src={Valores} alt="valores" className="w-24 h-24" />
-  //       </ContentIcon>
-  //       <ContentTitle>
-  //         <Title>Valores</Title>
-  //       </ContentTitle>
-  //       <ContentText>
-  //         <Text style={{textAlign: 'center'}}>
-  //           Confiança e Qualidade <br></br>
-  //           Segurança e sigilo nas informações <br></br>
-  //           Responsabilidade Social e ambiental <br></br>
-  //           Foco no Cliente e Ética Profissional <br></br>
-  //           Respeito aos clientes e stakeholders <br></br>
-  //           Respeito à diversidade e Flexibilidade<br></br>
-  //           Reconhecimento, valorização e qualificação das competências. <br></br>
-  //         </Text>
-  //       </ContentText>
-  //     </Card>
-  //     <Card>
-  //       <ContentIcon>
-  //         <img src={Visao} alt="visao" className="w-24 h-24" />
-  //       </ContentIcon>
-  //       <ContentTitle>
-  //         <Title>Visão</Title>
-  //       </ContentTitle>
-  //       <ContentText>
-  //         <Text>Oferecer soluções de excelência em logística integrada e transporte de cargas em todo estado da Bahia, viabilizando a distribuição dos materiais dos nossos clientes, dando visibilidade das nossas ações ao mercado.</Text>
-  //       </ContentText>
-  //     </Card>
-  //   </Container>
-  // )
   return (
     <div className="flex flex-wrap justify-center mt-8 sm:mt-16 md:mt-24">
       {/* Mission Card */}
--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,25 +1,50 @@
 import React, { useState } from "react";
 
 
+import Seculus from '../../assets/seculus.png';
+import Morena from '../../assets/Morena.png';
+import Propeg from '../../assets/propeg.png';
+import Braskem from '../../assets/braskem.png';
+import Pamcary from '../../assets/pamcary.png';
+import Ortobom from '../../assets/ortobom.png';
+
 const testimonialsData = [
   {
     id: 1,
-    name: 'João Silva',
-    image: '',
-    text: 'Excelente serviço! Recomendo a todos.',
+    name: 'Seculus',
+    image: Seculus,
+    text: 'Com a prestação de serviços da Log, tivemos mais agilidade na entrega e devolução dos nossos produtos. E o mais importante, a satisfação dos nossos clientes.  Qualidade e excelência na prestação de serviços.  Eficiência, Pontualidade e Excelência.',
   },
   {
     id: 2,
-    name: 'Maria Souza',
-    image: '',
-    text: 'Fiquei muito satisfeita com o atendimento.',
+    name: 'Morena',
+    image: Morena,
+    text: 'A Log Transportes atua nas empresas do Grupo há muitos anos e com ela conquistamos ainda maior agilidade e pontualidade nas entregas.   Normalmente descobrimos a qualidade de uma empresa, quando surge uma situação de crise. Sempre que tivemos uma falta de um motorista ou a necessidade de um acréscimo, mesmo que temporário, de serviço, a LOG sempre se mostrou solícita e efetiva, conquistando assim a credibilidade necessária para permanecer no Grupo.  Parceria, Pontualidade e Assiduidade.',
   },
   {
     id: 3,
-    name: 'Pedro Alves',
-    image: '',
-    text: 'Profissionais altamente qualificados.',
+    name: 'Braskem',
+    image: Braskem,
+    text: 'A confiabilidade nos serviços prestados profissionalismo da Equipe, confiabilidade, respeito e atenção aos clientes!',
   },
+  {
+    id: 4,
+    name: 'Propeg',
+    image: Propeg,
+    text: 'Entregas eficazes e pontuais. Disponibilidade imediata de atender nossas demandas. Empresa séria e parceira.  Comprometimento, agilidade, disponibilidade e seriedade na prestação do serviço, otimizando as demandas de nossa empresa.  Comprometimento, parceria e disponibilidade.',
+  },
+  {
+    id: 5,
+    name: 'Pamcary',
+    image: Pamcary,
+    text: 'Agilidade nas entregas de nossa carteira de clientes. O envolvimento direto das pessoas de comando que atuam em parceria conosco.  Agilidade, confiabilidade e competência.',
+  },
+  {
+    id: 6,
+    name: 'Ortobom',
+    image: Ortobom,
+    text: 'Na verdade já tínhamos este serviço porém era feito por funcionários próprios da fábrica. Com a LOG permanecemos com a mesma qualidade.  Bom serviço de logistica com custo beneficio adequado.  Qualidade, Pontualidade e Presteza.',
+  }
 ];
 
 export function Testimonials() {
@@ -80,16 +105,17 @@ export function Testimonials() {
             </svg>
           </span>
         </button>
+
         <div className="text-center">
           <p className="text-title text-2xl md:text-xl font-poppins">
             Depoimentos
           </p>
-          <div className="bg-white p-4 rounded-lg shadow-md">
-            <p className="text-base text-gray-500 font-normal font-poppins">
+          <div className="bg-white p-4 rounded-lg shadow-md flex flex-col justify-center items-center">
+            <p className="text-base text-text font-normal font-poppins">
               "{testimonialsData[currentIndex].text}"
             </p>
             <p className="font-poppins text-gray-600 mt-2">
-              - {testimonialsData[currentIndex].name}
+              <img src={testimonialsData[currentIndex].image} className="w-18 h-16" alt="Depoimento" />
             </p>
           </div>
         </div>
